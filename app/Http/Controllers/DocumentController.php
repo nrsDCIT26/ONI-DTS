b85f46b7ee8aeef4d904874985c734f439b8b52f
nrsDCIT26 <nrs.dcit26@gmail.com>
2024-03-19 15:40:01 +0800

    modified:   app/Http/Controllers/DocumentController.php 	modified:   resources/views/documents/show.blade.php

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -243,7 +243,7 @@ class DocumentController extends Controller
             $msg = "Rejected";
         } elseif ($action == 'approvef') {
             $this->documentRepository->approvedFDoc($document);
-            $msg = "Approved and Forwarded";
+            $msg = "Forwarded to Chief of Hospital" ."</i>";
         } elseif ($action == 'return') {
             $this->documentRepository->returnDoc($document);
             $msg = "Returned";
