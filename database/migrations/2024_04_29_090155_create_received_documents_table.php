2f47b721ec223597c926c30eb907363eb3ab9a9f
nrsDCIT26 <nrs.dcit26@gmail.com>
2024-04-29 13:49:24 +0800

    modified:   database/migrations/2024_04_29_090155_create_received_documents_table.php

--- database/migrations/2024_04_29_090155_create_received_documents_table.php
+++ database/migrations/2024_04_29_090155_create_received_documents_table.php
@@ -9,21 +9,21 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('received_documents', function (Blueprint $table) {
             $table->increments('id');
-            $table->unsignedBigInteger('document_id');
+            $table->integer('document_id')->unsigned();
             $table->unsignedBigInteger('creator_id');
             $table->unsignedBigInteger('sender_id');
-            $table->unsignedBigInteger('receiver_id');
-            $table->timestamps();
+            $table->integer('receiver_id')->unsigned();
+            $table->foreign('document_id')->references('id')->on('documents')->onDelete('cascade');
             $table->foreign('creator_id')->references('id')->on('users');
             $table->foreign('sender_id')->references('id')->on('users');
             $table->foreign('receiver_id')->references('id')->on('tags');
-            $table->foreign('document_id')->references('id')->on('documents')->onDelete('cascade');
+            $table->timestamps();
         });
-    }
+    }    
 
     /**
      * Reverse the migrations.
