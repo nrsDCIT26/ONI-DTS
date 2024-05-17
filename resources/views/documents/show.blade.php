a178b1eb9590c629ae4b9879ae607066e4647b7c
nrsDCIT26 <nrs.dcit26@gmail.com>
2024-05-17 16:49:03 +0800

    modified:   resources/views/documents/show.blade.php

--- resources/views/documents/show.blade.php
+++ resources/views/documents/show.blade.php
@@ -249,7 +249,7 @@
                         @endif
                         </div>
                         <div class="form-group">
-                            <label>Status:</label>0
+                            <label>Status:</label>
                             @if ($document->status==config('constants.STATUS.PENDING'))
                                 <span class="label label-warning">{{$document->status}}</span>
                             @elseif($document->status==config('constants.STATUS.APPROVED'))
@@ -262,13 +262,13 @@
                         </div>
                         <div class="form-group">
                             <label>Created At:</label>
-                            <p style="font-family: Varela Round; font-weight: regular; font-size: 12px">{{ date('F j, Y \a\t g:i A', strtotime($document->updated_at)) }} <br>
+                            <p style="font-family: Varela Round; font-weight: regular; font-size: 12px">{!! formatDateTime($document->created_at) !!} <br>
                                 ({{\Carbon\Carbon::parse($document->created_at)->diffForHumans()}})
                             </p>
                         </div>
                         <div class="form-group">
                             <label>Last Updated:</label>
-                            <p style="font-family: Varela Round; font-weight: regular; font-size: 12px;">{{ date('F j, Y \a\t g:i A', strtotime($document->updated_at)) }} <br>
+                            <p style="font-family: Varela Round; font-weight: regular; font-size: 12px;">{!! formatDateTime($document->updated_at) !!} <br>
                                 ({{\Carbon\Carbon::parse($document->updated_at)->diffForHumans()}})
                             </p> 
                         </div>
