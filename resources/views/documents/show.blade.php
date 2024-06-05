5a5071804c3fdf988cee7295c37c90bdd3a8ff3c
nrsDCIT26 <nrs.dcit26@gmail.com>
2024-06-05 09:56:44 +0800

    modified:   resources/views/documents/show.blade.php

--- resources/views/documents/show.blade.php
+++ resources/views/documents/show.blade.php
@@ -335,11 +335,11 @@
                                                         by <b>{{$file->createdBy->name}}</b></small>
                                                 </div>
                                                 <div class="pull-right box-tools">
-                                                    @if ($document->status != config('constants.STATUS.APPROVED'))
+                                                    @if ($document->status == config('constants.STATUS.APPROVED'))
                                                     <button type="button"
                                                             class="btn btn-default btn-flat dropdown-toggle"
                                                             data-toggle="dropdown" aria-expanded="false"
-                                                            style="    background: transparent;border: none;">
+                                                            style="    background: transparent;border: none;"> 
                                                         <i class="fa fa-ellipsis-v" style="color: #fff;"></i>
                                                         <span class="sr-only">Toggle Dropdown</span>
                                                     </button>
